5600a520af8329c0170703a6d5c01d12295fb303
Lucas Tran <lucastran1107@gmail.com>
2024-01-22 20:04:53 +0100

    feat: :sparkles: Continue working on Navbar
    Add more elements and a search bar to the navbar, and style the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,25 +1,52 @@
-"use client"
-import  Link  from "next/link";
-import { cn } from "@/lib/utils"
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 function Navbar() {
-
   return (
-    <nav className="navbar">
-            <div className="flex items-center gap-6" >
-                <Link href="/" className={cn(
-                  "transition-colors hover:text-foreground/80",
-          )}>
-                    <p>Side1</p>
-                </Link>
-            </div>
+    <nav className="flex items-center justify-between p-3 bg-slate-700 text-white w-full">
+      <div>
+        <Link href="/">
+          <p className={cn("transition-colors hover:bg-slate-500 p-3", "cursor-pointer")}>
+            Logo
+          </p>
+        </Link>
+      </div>
 
-            <ul className="navbar-list">
-                <Link href='/'><li className="navbar-item">Side2</li></Link>
-            </ul>
-            
-        </nav>
-  )
+      <div className="flex items-center flex-grow justify-center gap-10">
+        <Link href="/">
+          <p className={cn("transition-colors hover:bg-slate-500 p-3", "cursor-pointer")}>
+            Page 1
+          </p>
+        </Link>
+
+        <Link href="/">
+          <p className={cn("transition-colors hover:bg-slate-500 p-3", "cursor-pointer")}>
+            Page 2
+          </p>
+        </Link>
+
+        <Link href="/">
+          <p className={cn("transition-colors hover:bg-slate-500 p-3", "cursor-pointer")}>
+            Page 3
+          </p>
+        </Link>
+
+        <Link href="/">
+          <p className={cn("transition-colors hover:bg-slate-500 p-3", "cursor-pointer")}>
+            Page 4
+          </p>
+        </Link>
+      </div>
+
+      <div className="ml-auto">
+          <input
+            type="text"
+            placeholder="Search"
+            className="p-1 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-black"
+          />
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
