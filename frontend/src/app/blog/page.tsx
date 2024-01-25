3530ac50e074e63d8e6846aa0598f4869535d736
Thrywyn <magnus.stromseng@gmail.com>
2024-01-25 19:40:21 +0100

    feat(frontend): :sparkles: fetch example blog data from strapi

--- frontend/src/app/blog/page.tsx
+++ frontend/src/app/blog/page.tsx
@@ -1,7 +1,32 @@
-export default function BlogPage() {
+async function getData() {
+    const res = await fetch("http://localhost:1337/api/articles", {
+        cache: "no-cache", // Disable caching during development
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch data:" + res.statusText);
+    }
+
+    return res.json();
+}
+
+export default async function BlogPage() {
+    const response = await getData();
+    const data = response.data;
+
+    console.log(response.data);
+
     return (
         <div>
             <h1>Blog</h1>
+            {data.map((post: any) => {
+                return (
+                    <div key={post.id}>
+                        <h2>{post.attributes.Title}</h2>
+                        <p>{post.attributes.Body[0].children[0].text}</p>
+                    </div>
+                );
+            })}
         </div>
     );
 }
