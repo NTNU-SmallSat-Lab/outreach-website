454e5cce3ee077945363d0a95b8bce5e6b6d05c0
Lucas Tran <lucastran1107@gmail.com>
2024-01-26 18:23:27 +0100

    feat(frontend): :sparkles: Create content on partner's page
    Add hardcoded content to partner's page and styled a tiny bit for better user experience.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ['www.ntnu.edu'], // Add your domain(s) here
+    },
+  };
+  
 
 export default nextConfig;
