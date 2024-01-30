59f1dc4f21a342c7537cfaf3f0781e82d9335347
Lukas <lukas@thrane.name>
2024-01-30 04:08:45 +0100

    feat(frontend): :poop: Try to fix styling

--- frontend/src/app/(about)/infrastructure/page.tsx
+++ frontend/src/app/(about)/infrastructure/page.tsx
@@ -1,7 +1,7 @@
-import ReactMarkdown from "react-markdown";
 import { getClient } from "@/lib/ApolloClient";
 import { gql } from "@/__generated__/gql";
 import { BlocksContent, BlocksRenderer } from "@strapi/blocks-react-renderer";
+
 const GET_STRUCTURE = gql(`
 query GET_STRUCTURE {
     infrastructure {
@@ -14,7 +14,7 @@ query GET_STRUCTURE {
   }
 `);
 
-export default async function PartnersPage() {
+export default async function InfrastructurePage() {
     const graphqlData = await getClient().query({
         query: GET_STRUCTURE,
     });
@@ -22,12 +22,23 @@ export default async function PartnersPage() {
     const content: BlocksContent =
         graphqlData?.data?.infrastructure?.data?.attributes?.Content ?? [];
 
+    console.log(content);
+
     return (
         <div className="flex flex-col items-center text-center">
-            <h1 className="text-3xl font-bold mb-10 mt-5">
-                Infrastructure
-            </h1>
+            <h1 className="text-3xl font-bold mb-10 mt-5">Infrastructure</h1>
             <BlocksRenderer content={content} />
+            <hr />
+            <h1 className="text-3xl font-bold mb-10 mt-5">Content in JSON</h1>
+            {content.map((content) => {
+                console.log(content);
+                return (
+                    <div>
+                        <h1>{content.type}</h1>
+                        <p>{JSON.stringify(content)}</p>
+                    </div>
+                );
+            })}
         </div>
     );
 }
