ca8034c3b3784b300034614285c4526eab027699
Thrywyn <magnus.stromseng@gmail.com>
2024-02-15 20:43:48 +0100

    refactor(frontend): :fire: remove unused import

--- frontend/src/components/map/MyCustomMap.tsx
+++ frontend/src/components/map/MyCustomMap.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import "ol/ol.css";
 import Map from "ol/Map";
 import View from "ol/View";
