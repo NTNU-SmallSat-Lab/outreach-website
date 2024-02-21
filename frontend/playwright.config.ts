35a2e6d15801999396c4c9e40472559bb7a9e0d3
Jakobgg1243 <Jakob.gregusson@gmail.com>
2024-02-21 13:02:00 +0100

    test(frontend): :white_check_mark: Add webserver for playwright

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -9,6 +9,7 @@ import { defineConfig, devices } from "@playwright/test";
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+const baseURL = `http://localhost:3000`;
 export default defineConfig({
     testDir: "./tests",
     /* Run tests in files in parallel */
@@ -24,19 +25,28 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: "https://b70ca98b.it2901-smallsatlab-hypso.pages.dev/",
+        baseURL,
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
     },
+    webServer: [
+        {
+            command: "npm run dev",
+            url: baseURL,
+            timeout: 120 * 1000,
+            reuseExistingServer: !process.env.CI,
+        },
+        {
+            command: "cd ../backend && npm run develop",
+            url: "http://localhost:1337",
+            timeout: 120 * 1000,
+            reuseExistingServer: !process.env.CI,
+        },
+    ],
 
     /* Configure projects for major browsers */
     projects: [
-        {
-            name: "chromium",
-            use: { ...devices["Desktop Chrome"] },
-        },
-
         {
             name: "firefox",
             use: { ...devices["Desktop Firefox"] },
@@ -62,9 +72,9 @@ export default defineConfig({
         //   name: 'Microsoft Edge',
         //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
         // },
-        // {
-        //   name: 'Google Chrome',
-        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-        // },
+        {
+            name: "Google Chrome",
+            use: { ...devices["Desktop Chrome"], channel: "chrome" },
+        },
     ],
 });
