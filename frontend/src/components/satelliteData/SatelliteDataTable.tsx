6ec81fcf963223821963e00bdd4800467462d7e5
Lukas Thrane <lukas@thrane.name>
2024-02-26 17:46:21 +0100

    add working position to table

--- frontend/src/components/satelliteData/SatelliteDataTable.tsx
+++ frontend/src/components/satelliteData/SatelliteDataTable.tsx
@@ -1,7 +1,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { exampleData } from "../map/exampleSatData";
-import { SatelliteData, mapRawDataToSatData, mapRawDataToTleData } from "@/lib/mapHelpers";
+import {
+    SatelliteData,
+    mapRawDataToSatData,
+    mapRawDataToTleData,
+} from "@/lib/mapHelpers";
 import {
     Table,
     TableBody,
@@ -14,19 +18,8 @@ import {
 } from "@/components/ui/table";
 import * as satellite from "satellite.js";
 
-const SATELLITE_AMOUNT = 10; // amount of satellites to display
-
 export default function SatelliteDataTable() {
-    const [satelliteDataWithCoords, setSatelliteDataWithCoords] = useState([]);
-
-    useEffect(() => {
-        let satDatas = [];
-        satDatas = mapRawDataToSatData(exampleData)
-
-        const date = new Date()
-
-
-    }, []);
+    var satData = mapRawDataToSatData(exampleData).slice(0, 10);
 
     return (
         <div className="flex flex-col justify-center items-center m-10 w-full">
@@ -40,14 +33,52 @@ export default function SatelliteDataTable() {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {satelliteDataWithCoords.map((data, index) => {
-                        return (
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>Latitude</TableCell>
-                                <TableCell>Longitude</TableCell>
-                            </TableRow>
+                    {satData.map((data, index) => {
+                        var positionAndVelocity = satellite.propagate(
+                            data.satrec,
+                            new Date(),
                         );
+
+                        // Check if position is not false before proceeding
+                        if (
+                            positionAndVelocity.position &&
+                            typeof positionAndVelocity.position !== "boolean"
+                        ) {
+                            var gmst = satellite.gstime(new Date()); // Greenwich Mean Sidereal Time
+                            var positionGd = satellite.eciToGeodetic(
+                                positionAndVelocity.position,
+                                gmst,
+                            );
+
+                            // Convert radians to degrees for latitude and longitude
+                            var latitudeDeg = satellite.degreesLat(
+                                positionGd.latitude,
+                            );
+                            var longitudeDeg = satellite.degreesLong(
+                                positionGd.longitude,
+                            );
+
+                            return (
+                                <TableRow key={index}>
+                                    <TableCell>{data.name}</TableCell>
+                                    <TableCell>
+                                        {latitudeDeg.toFixed(2)}
+                                    </TableCell>
+                                    <TableCell>
+                                        {longitudeDeg.toFixed(2)}
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        } else {
+                            // Handle the case where propagation failed or returned false
+                            return (
+                                <TableRow key={index}>
+                                    <TableCell>{data.name}</TableCell>
+                                    <TableCell>N/A</TableCell>
+                                    <TableCell>N/A</TableCell>
+                                </TableRow>
+                            );
+                        }
                     })}
                 </TableBody>
             </Table>
