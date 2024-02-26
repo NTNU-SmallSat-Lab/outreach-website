36faa13c35953725fcd1c397e575a2d4c59e6b4d
Mads Hermansen <119772939+madshermansen@users.noreply.github.com>
2024-02-26 15:06:04 +0100

    style(frontend): :art: Setup Color Config in Tailwind + example on homepage

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -22,12 +22,6 @@ const config = {
                 border: "hsl(var(--border))",
                 input: "hsl(var(--input))",
                 ring: "hsl(var(--ring))",
-                background: "hsl(var(--background))",
-                foreground: "hsl(var(--foreground))",
-                primary: {
-                    DEFAULT: "hsl(var(--primary))",
-                    foreground: "hsl(var(--primary-foreground))",
-                },
                 secondary: {
                     DEFAULT: "hsl(var(--secondary))",
                     foreground: "hsl(var(--secondary-foreground))",
@@ -40,10 +34,6 @@ const config = {
                     DEFAULT: "hsl(var(--muted))",
                     foreground: "hsl(var(--muted-foreground))",
                 },
-                accent: {
-                    DEFAULT: "hsl(var(--accent))",
-                    foreground: "hsl(var(--accent-foreground))",
-                },
                 popover: {
                     DEFAULT: "hsl(var(--popover))",
                     foreground: "hsl(var(--popover-foreground))",
@@ -52,6 +42,20 @@ const config = {
                     DEFAULT: "hsl(var(--card))",
                     foreground: "hsl(var(--card-foreground))",
                 },
+
+                // Small Satellite Lab Hypso colors
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: 'hsl(var(--ntnuBlue))',
+                accent: 'hsl(var(--accent))',
+                ntnuBlue: 'hsl(var(--ntnuBlue))',
+                ntnuCyan: 'hsl(var(--ntnuCyan))',
+                ntnuPurple: 'hsl(var(--ntnuPurple))',
+                ntnuOrange: 'hsl(var(--ntnuOrange))',
+                ntnuYellow: 'hsl(var(--ntnuYellow))',
+                ntnuLightblue: 'hsl(var(--ntnuLightblue))',
+                ntnuPink: 'hsl(var(--ntnuPink))',
+                ntnuBrown: 'hsl(var(--ntnuBrown))',
             },
             borderRadius: {
                 lg: "var(--radius)",
