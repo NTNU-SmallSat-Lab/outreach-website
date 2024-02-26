b54833f3dcd82968f950fe177a8a656693ba1db0
EliasKnudsen <eliasskn@stud.ntnu.no>
2024-02-26 20:11:49 +0100

    feat: :sparkles: Docker compose with github runner

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -4,11 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "sqlite",
     connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        env("DATABASE_FILENAME", ".tmp/data.db")
-      ),
+      filename: env("DATABASE_FILENAME", "/tmp/strapi.db")
     },
     useNullAsDefault: true,
   },
