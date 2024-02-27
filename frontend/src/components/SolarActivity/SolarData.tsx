d611fd9e2f1fff4de45d66b6b564e7526891b11a
Lukas <lukas@thrane.name>
2024-02-27 21:53:36 +0100

    graph

--- frontend/src/components/SolarActivity/SolarData.tsx
+++ frontend/src/components/SolarActivity/SolarData.tsx
@@ -1,8 +1,14 @@
-"use client";
-import React, { useEffect } from "react";
-import { Chart } from "chart.js";
+import React, { useEffect, useRef, useState } from "react";
+import { Chart, registerables } from "chart.js";
 
-async function getSolarData() {
+interface SolarDataEntry {
+    [0]: string;
+    [1]: string;
+}
+
+type SolarData = SolarDataEntry[];
+
+async function getSolarData(): Promise<SolarData> {
     const res = await fetch(
         "https://services.swpc.noaa.gov/products/noaa-planetary-k-index-forecast.json",
     );
@@ -15,15 +21,53 @@ async function getSolarData() {
 }
 
 export default function SolarData() {
+    const [solarData, setSolarData] = useState<SolarData | null>(null);
+    const chartRef = useRef<HTMLCanvasElement | null>(null);
+
     useEffect(() => {
         getSolarData()
-            .then((data) => console.log(data))
+            .then((data) => setSolarData(data))
             .catch((err) => console.error(err));
     }, []);
 
+    useEffect(() => {
+        const solarDataChart = chartRef.current;
+
+        if (
+            solarData &&
+            solarDataChart &&
+            solarDataChart instanceof HTMLCanvasElement
+        ) {
+            Chart.register(...registerables);
+
+            const chart = new Chart(solarDataChart, {
+                type: "line",
+                data: {
+                    labels: solarData.map((data) => data[0]),
+                    datasets: [
+                        {
+                            label: "Kp Index",
+                            data: solarData.map((data) => +data[1]),
+                            fill: false,
+                            borderColor: "rgb(75, 192, 192)",
+                            tension: 0.1,
+                        },
+                    ],
+                },
+            });
+
+            return () => {
+                chart.destroy();
+            };
+        }
+    }, [solarData]);
+
     return (
-        <>
-            <h1>Solar Data</h1>
-        </>
+        <div>
+            <h2>Solar Activity Data</h2>
+            <canvas ref={chartRef}></canvas>
+            <hr />
+            <pre>{JSON.stringify(solarData, null, 2)}</pre>
+        </div>
     );
 }
