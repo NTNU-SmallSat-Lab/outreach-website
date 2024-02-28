2574471206269b37622dbe1d81e45da12af3353b
Lukas <lukas@thrane.name>
2024-02-28 15:23:21 +0100

    run lint and prettier

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     return (
         <main>
             <SatelliteDataTable />
-            
+
             <SatelliteFetcher useExampleData={true} />
 
             <MyCustomMap />
