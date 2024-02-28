feb7b4331a0e086dce236c0a8411d293cbcb5022
Mads Hermansen <119772939+madshermansen@users.noreply.github.com>
2024-02-28 13:38:57 +0100

    feat(frontend): :sparkles: Add responsive navbar for mobile

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,6 +10,22 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { useTheme } from "next-themes";
 import { Icon } from "@iconify/react";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
+
+import type { SVGProps } from 'react';
+
+export function IconParkHamburgerButton(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={40} height={40} viewBox="0 0 48 48" {...props}><g fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth={4}><path d="M7.94971 11.9497H39.9497"></path><path d="M7.94971 23.9497H39.9497"></path><path d="M7.94971 35.9497H39.9497"></path></g></svg>);
+}
 
 export default function Navbar() {
     const dropdownPaths = [
@@ -54,8 +70,24 @@ export default function Navbar() {
                     </h1>
                 </Link>
             </div>
+            <Drawer>
+            <DrawerTrigger  className="sm:hidden flex justify-end"><IconParkHamburgerButton /></DrawerTrigger>
+            <DrawerContent>
+                <DrawerHeader>
+                <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                </DrawerHeader>
+                <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose>
+                    <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+                </DrawerFooter>
+            </DrawerContent>
+            </Drawer>
 
-            <div className="flex-1 flex justify-center">
+
+            <div className="flex-1 justify-center hidden sm:flex">
                 <div className="flex items-center gap-8">
                     <Link href="/blog">
                         <Button variant={getButtonVariant("/blog")}>
