1aa9eb4d34a6966b0b15c4cca013ba61f7084f64
Lucas Tran <lucastran1107@gmail.com>
2024-02-28 11:32:03 +0100

    fix(frontend): :rotating_light: Fix lint error

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -83,7 +83,7 @@ export default async function Home() {
                                             </h1>
                                         )}
                                     </CardContent>
-                                </Card><
+                                </Card>
                             </div>
                         </Link>
                     );
