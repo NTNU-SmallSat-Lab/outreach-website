63873e3370c0b2e3fcbcd4c1b4df5e8d13ebb06a
Mads Hermansen <119772939+madshermansen@users.noreply.github.com>
2024-03-17 19:51:47 +0100

    refactor: :memo: run prettier and fix linting

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,21 +3,32 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
-import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
-} from "@/components/ui/drawer";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 
-import type { SVGProps } from 'react';
+import type { SVGProps } from "react";
 
 export function IconParkHamburgerButton(props: SVGProps<SVGSVGElement>) {
-	return (<svg xmlns="http://www.w3.org/2000/svg" width={40} height={40} viewBox="0 0 48 48" {...props}><g fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth={4}><path d="M7.94971 11.9497H39.9497"></path><path d="M7.94971 23.9497H39.9497"></path><path d="M7.94971 35.9497H39.9497"></path></g></svg>);
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={40}
+            height={40}
+            viewBox="0 0 48 48"
+            {...props}
+        >
+            <g
+                fill="none"
+                stroke="white"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={4}
+            >
+                <path d="M7.94971 11.9497H39.9497"></path>
+                <path d="M7.94971 23.9497H39.9497"></path>
+                <path d="M7.94971 35.9497H39.9497"></path>
+            </g>
+        </svg>
+    );
 }
 
 export default function Navbar() {
@@ -51,35 +62,40 @@ export default function Navbar() {
         <nav className="flex w-full items-center justify-between border-b border-x-neutral-600 bg-background p-4 text-foreground">
             <div className="flex-1">
                 <Link href="/">
-                    <img src="/images/ntnu-white-logo.svg" alt="logo" className="min-w-64 w-64" />
+                    <img
+                        src="/images/ntnu-white-logo.svg"
+                        alt="logo"
+                        className="w-64 min-w-64"
+                    />
                 </Link>
             </div>
             <Drawer>
-            <DrawerTrigger  className="sm:hidden flex justify-end"><IconParkHamburgerButton /></DrawerTrigger>
-            <DrawerContent>
-                <div className="flex flex-col items-center gap-8 justify-center align-center">
-                    <Link href="/blog">
-                        <Button variant={getButtonVariant("/blog")}>
-                            Blog
-                        </Button>
-                    </Link>
+                <DrawerTrigger className="flex justify-end sm:hidden">
+                    <IconParkHamburgerButton />
+                </DrawerTrigger>
+                <DrawerContent>
+                    <div className="align-center flex flex-col items-center justify-center gap-8">
+                        <Link href="/blog">
+                            <Button variant={getButtonVariant("/blog")}>
+                                Blog
+                            </Button>
+                        </Link>
 
-                    <Link href="/projects">
-                        <Button variant={getButtonVariant("/projects")}>
-                            Projects
-                        </Button>
-                    </Link>
-                    <Link href="/satellites">
-                        <Button variant={getButtonVariant("/satellites")}>
-                            Satellites
-                        </Button>
-                    </Link>
-                </div>
-            </DrawerContent>
+                        <Link href="/projects">
+                            <Button variant={getButtonVariant("/projects")}>
+                                Projects
+                            </Button>
+                        </Link>
+                        <Link href="/satellites">
+                            <Button variant={getButtonVariant("/satellites")}>
+                                Satellites
+                            </Button>
+                        </Link>
+                    </div>
+                </DrawerContent>
             </Drawer>
 
-
-            <div className="flex-1 justify-center hidden sm:flex">
+            <div className="hidden flex-1 justify-center sm:flex">
                 <div className="flex items-center gap-8">
                     <Link href="/blog">
                         <Button variant={getButtonVariant("/blog")}>
--- frontend/src/components/ui/drawer.tsx
+++ frontend/src/components/ui/drawer.tsx
@@ -45,7 +45,7 @@ const DrawerContent = React.forwardRef<
         <DrawerPrimitive.Content
             ref={ref}
             className={cn(
-                "fixed inset-x-0 top-0 z-50 pb-3 pt-3 flex h-auto flex-col rounded-t-[10px] border bg-background",
+                "fixed inset-x-0 top-0 z-50 flex h-auto flex-col rounded-t-[10px] border bg-background pb-3 pt-3",
                 className,
             )}
             {...props}
