af1e1a8b8c42572041b913074657bcfb09c914c5
Thrywyn <magnus.stromseng@gmail.com>
2024-03-18 14:35:19 +0100

    install and configure graphql plugin

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = () => ({
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 20,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
