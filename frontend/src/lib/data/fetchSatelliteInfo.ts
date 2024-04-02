5b0245aba63c418040df89b66391cac8064cd583
Mads Hermansen <119772939+madshermansen@users.noreply.github.com>
2024-04-02 20:07:14 +0200

    132 style individual satellite page (#187)
    * feat(frontend): :sparkles: add image support gql query as well as styling changes
    
    * Refactor previewImage assignment in fetchSatelliteInfo.ts
    
    * Update fetchSatelliteInfo to handle missing previewImage
    
    * Refactor code to improve performance and readability
    
    * feat: 180-fikse-colored-background-sections-on-forntpage-maybe-seethrough-but-more-dark-idk
    
    * fix: Refactor previewImage URL assignment in fetchSatelliteInfo.ts

--- frontend/src/lib/data/fetchSatelliteInfo.ts
+++ frontend/src/lib/data/fetchSatelliteInfo.ts
@@ -1,37 +1,46 @@
 import { gql } from "@/__generated__/gql";
+const HOST_URL = process.env.HOST_URL;
+
 import { Projects, SatelliteInfo } from "@/app/satellites/[satelliteSlug]/page";
 import { getClient } from "../ApolloClient";
 
 const GET_SATELLITE_INFO =
     gql(`query GET_SATELLITE_INFO($filters: SatelliteFiltersInput) {
-        satellites(filters: $filters) {
-          data {
-              id
-              attributes {
-                  celestrakURL
-                  catalogNumberNORAD
-                  content
-                  name
-                projects {
-                  data {
-                    attributes {
-                      title
-                      coverImage {
-                        data {
-                          attributes {
-                            url
-                          }
+      satellites(filters: $filters) {
+        data {
+            id
+            attributes {
+                celestrakURL
+                catalogNumberNORAD
+                content
+                name
+              projects {
+                data {
+                  attributes {
+                    title
+                    coverImage {
+                      data {
+                        attributes {
+                          url
                         }
                       }
-                      slug
                     }
-                    id
+                    slug
+                  }
+                  id
+                }
+              }
+              previewImage {
+                data {
+                  attributes {
+                    url
                   }
                 }
               }
-          }
+            }
         }
       }
+    }
   `);
 
 export default async function fetchSatelliteInfo({
@@ -68,10 +77,18 @@ export default async function fetchSatelliteInfo({
         },
     );
 
+    let previewImage =
+        graphqlData?.data?.satellites?.data[0]?.attributes?.previewImage?.data
+            ?.attributes?.url ?? "";
+    if (HOST_URL && previewImage != undefined) {
+        previewImage = HOST_URL + previewImage;
+    }
+
     satelliteInfo = {
         name: graphqlData?.data?.satellites?.data[0]?.attributes?.name ?? "",
         content:
             graphqlData?.data?.satellites?.data[0]?.attributes?.content ?? "",
+        imageURL: previewImage ?? "",
         relatedProjects: projects ?? [],
     };
 
