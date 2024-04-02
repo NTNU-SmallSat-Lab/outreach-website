7ba23fc0293e7dd2e424e5a0217262f7e8a58b35
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-04-02 19:45:57 +0200

    position globe in grid and resize it automatically

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,14 +8,8 @@ const HOST_URL = process.env.HOST_URL;
 import Image from "next/image";
 import Link from "next/link";
 
-// Dynamic import because of leaflet and globe.gl ssr problem with next.js
-import dynamic from "next/dynamic";
 import SatelliteFetcher from "@/components/map/SatelliteFetcher";
 import SatelliteDataTable from "@/components/satelliteData/SatelliteDataTable";
-
-const MyCustomMap = dynamic(() => import("@/components/map/MyCustomMap"), {
-    ssr: false,
-});
 
 const GET_MOST_RECENT_IMAGE = gql(`
 query MostRecentImages {
@@ -62,19 +56,19 @@ export default async function Home() {
     }
 
     return (
-        <main>
+        <>
             <div className="grid grid-cols-2">
-                <SatelliteDataTable satName="HYPSO-1" />
-                <SatelliteDataTable satName="UME (ISS)" />
-                <SatelliteDataTable satName="STARLINK-1007" />
-                <SatelliteDataTable satName="VANGUARD 1" />
-                <SatelliteDataTable satName="MULTIFUNCTION TEST SAT" />
-                <SatelliteDataTable satName="huh" />
+                <div className="grid grid-cols-2">
+                    <SatelliteDataTable satName="HYPSO-1" />
+                    <SatelliteDataTable satName="UME (ISS)" />
+                    <SatelliteDataTable satName="STARLINK-1007" />
+                    <SatelliteDataTable satName="VANGUARD 1" />
+                    <SatelliteDataTable satName="MULTIFUNCTION TEST SAT" />
+                    <SatelliteDataTable satName="huh" />
+                </div>
+
+                <SatelliteFetcher useExampleData={true} />
             </div>
-
-            <SatelliteFetcher useExampleData={true} />
-
-            <MyCustomMap />
 
             <ColoredSection
                 id="about-us"
@@ -148,6 +142,6 @@ export default async function Home() {
                     </div>
                 </div>
             </ColoredSection>
-        </main>
+        </>
     );
 }
