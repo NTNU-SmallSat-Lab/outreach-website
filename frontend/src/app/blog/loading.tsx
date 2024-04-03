18476faa8ac63a5cb3739fb1a5d5c799ebcc2668
Mats <mnyflot@gmail.com>
2024-04-03 16:28:48 +0200

    feat(frontend): :sparkles: add loading skeleton (unfinished) to blogs page

--- frontend/src/app/blog/loading.tsx
+++ frontend/src/app/blog/loading.tsx
@@ -1,14 +1,54 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import {
+    EmptySection,
+    Skeleton,
+    SkeletonHero,
+    SkeletonSection,
+} from "@/components/ui/skeleton";
+import React from "react";
 
-export default function BlogsLoadingSkeleton() {
-  return(
-    <div>
-        <Skeleton className="w-[56px] max-w-full" />
-        <Skeleton className="w-[56px] max-w-full" />
-        <Skeleton className="w-[100px] max-w-full" />
-        <Skeleton className="w-[56px] max-w-full" />
-        <Skeleton className="w-[56px] max-w-full" />
-    </div>
-);
-
+export default function Loading() {
+    return (
+        <div>
+            {/* Full width div, split into two equal portions */}
+            <div className="flex w-full flex-col-reverse gap-5 p-4 md:h-[calc(100vh-73px)] md:flex-row">
+                {/* Square */}
+                <div className="flex h-full w-full flex-col gap-2">
+                    <div className="flex flex-row gap-2">
+                        <Skeleton className="h-24 w-1/2" />
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <Skeleton className="h-24 w-full" />
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <Skeleton className="h-24 w-full" />
+                    </div>
+                </div>
+                <div className="flex h-full w-full flex-col gap-2">
+                    <div className="flex flex-row gap-2">
+                        <Skeleton className="h-24 w-1/2" />
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <Skeleton className="h-24 w-full" />
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <Skeleton className="h-24 w-full" />
+                    </div>
+                </div>
+                <div className="flex h-full w-full flex-col gap-2">
+                    <div className="flex flex-row gap-2">
+                        <Skeleton className="h-24 w-1/2" />
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <Skeleton className="h-24 w-full" />
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <Skeleton className="h-24 w-full" />
+                    </div>
+                </div>
+            </div>
+            <SkeletonHero />
+            <SkeletonSection />
+            <EmptySection />
+        </div>
+    );
 }
--- frontend/src/components/ui/skeleton.tsx
+++ frontend/src/components/ui/skeleton.tsx
@@ -13,4 +13,35 @@ function Skeleton({
     );
 }
 
-export { Skeleton };
+function EmptySection({
+    children,
+    className,
+}: {
+    children?: React.ReactNode;
+    className?: string;
+}) {
+    return <div className={cn("h-dvh w-full", className)}> {children}</div>;
+}
+
+function SkeletonSection() {
+    return <Skeleton className="h-dvh w-full" />;
+}
+
+function SkeletonHero() {
+    return (
+        <div className="w-full py-64">
+            <div className="px-4">
+                <div className="flex flex-col items-center justify-center space-y-4">
+                    {/* Large Hero Text */}
+                    <Skeleton className="h-48 w-2/3" />
+                    {/* Small sub text */}
+                    <Skeleton className="h-4 w-1/3" />
+                    <Skeleton className="h-4 w-1/3" />
+                    <Skeleton className="h-4 w-1/3" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export { Skeleton, EmptySection, SkeletonSection, SkeletonHero };
