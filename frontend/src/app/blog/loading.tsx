63928d592f8bb5924342ff46aefa9c331e3a135c
Mats <mnyflot@gmail.com>
2024-04-03 17:02:59 +0200

    feat(frontend): :sparkles: blogs loading skeleton
    Co-authored-by: Mads Hermansen <madshermansen@users.noreply.github.com>

--- frontend/src/app/blog/loading.tsx
+++ frontend/src/app/blog/loading.tsx
@@ -9,41 +9,21 @@ import React from "react";
 export default function Loading() {
     return (
         <div>
-            {/* Full width div, split into two equal portions */}
-            <div className="flex w-full flex-col-reverse gap-5 p-4 md:h-[calc(100vh-73px)] md:flex-row">
-                {/* Square */}
-                <div className="flex h-full w-full flex-col gap-2">
-                    <div className="flex flex-row gap-2">
-                        <Skeleton className="h-24 w-1/2" />
-                    </div>
-                    <div className="flex flex-row gap-2">
-                        <Skeleton className="h-24 w-full" />
-                    </div>
-                    <div className="flex flex-row gap-2">
-                        <Skeleton className="h-24 w-full" />
-                    </div>
+            {/* Full width div, split into three equal portions */}
+            <div className="flex flex-col">
+                <div className="w-full">
+                    <Skeleton></Skeleton>
                 </div>
-                <div className="flex h-full w-full flex-col gap-2">
-                    <div className="flex flex-row gap-2">
-                        <Skeleton className="h-24 w-1/2" />
-                    </div>
-                    <div className="flex flex-row gap-2">
-                        <Skeleton className="h-24 w-full" />
-                    </div>
-                    <div className="flex flex-row gap-2">
-                        <Skeleton className="h-24 w-full" />
-                    </div>
+                <div className="w-full">
+                    <Skeleton></Skeleton>
                 </div>
-                <div className="flex h-full w-full flex-col gap-2">
-                    <div className="flex flex-row gap-2">
-                        <Skeleton className="h-24 w-1/2" />
-                    </div>
-                    <div className="flex flex-row gap-2">
-                        <Skeleton className="h-24 w-full" />
-                    </div>
-                    <div className="flex flex-row gap-2">
-                        <Skeleton className="h-24 w-full" />
-                    </div>
+                <div className="flex flex-row flex-wrap">
+                    <Skeleton></Skeleton>
+                    <Skeleton></Skeleton>
+                    <Skeleton></Skeleton>
+                    <Skeleton></Skeleton>
+                    <Skeleton></Skeleton>
+                    <Skeleton></Skeleton>
                 </div>
             </div>
             <SkeletonHero />
