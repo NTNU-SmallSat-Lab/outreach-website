e966e8523a020f0d438d7d23557a75c555734569
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-04-08 16:40:07 +0200

    fix(frontend): fix key prop warning

--- frontend/src/app/blog/blogDataCards.tsx
+++ frontend/src/app/blog/blogDataCards.tsx
@@ -2,6 +2,7 @@ export const runtime = "edge";
 import FullBlogCard from "@/components/fullBlogCard";
 import BlogpageButtons from "@/components/BlogpageButtons";
 import { BlogPost } from "./page";
+import React from "react";
 
 export default async function BlogDataCards({
     articles,
@@ -21,23 +22,28 @@ export default async function BlogDataCards({
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
                 <BlogpageButtons className="col-span-full" />
                 {/* Only map fist article */}
-                {articles.map((article: any) => {
+                {articles.map((article: BlogPost) => {
                     if (article.firstArticle) {
                         return (
-                            <FullBlogCard
-                                className="col-span-full"
-                                article={article}
-                                key={article.ID}
-                            />
+                            <React.Fragment key={article.key}>
+                                <FullBlogCard
+                                    className="col-span-full"
+                                    article={article}
+                                />
+                            </React.Fragment>
                         );
                     }
                 })}
 
-                {articles.map((article: any) => {
+                {articles.map((article: BlogPost) => {
                     if (article.firstArticle) {
                         return;
                     }
-                    return <FullBlogCard article={article} key={article.ID} />;
+                    return (
+                        <React.Fragment key={article.key}>
+                            <FullBlogCard article={article} />
+                        </React.Fragment>
+                    );
                 })}
             </div>
         </>
