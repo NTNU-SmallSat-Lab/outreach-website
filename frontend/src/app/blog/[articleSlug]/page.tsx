1c7d6851a753760af6b289f5dbc4104845dac7be
Mats Nyfløt <mnyflot@gmail.com>
2024-04-08 03:27:51 +0200

    style(frontend): :lipstick: Fit content into blogcard and add image

--- frontend/src/app/blog/[articleSlug]/page.tsx
+++ frontend/src/app/blog/[articleSlug]/page.tsx
@@ -5,12 +5,15 @@ import { BlocksContent } from "@strapi/blocks-react-renderer";
 import BlockRendererClient from "@/components/BlockRendererClient";
 import { gql } from "@/__generated__/gql";
 import { getClient } from "@/lib/ApolloClient";
+import { BlogCard } from "@/components/ui/blogCard";
+import Image from "next/image";
 const HOST_URL = process.env.HOST_URL;
 
 const GET_ARTICLE_BY_SLUG = gql(
     `query ArticleWithSlug($articlesFilters: ArticleFiltersInput) {
     articles(filters: $articlesFilters) {
       data {
+        id
         attributes {
           author {
             data {
@@ -81,8 +84,26 @@ export default async function Page({
     const datePublished = article?.attributes?.datePublished;
     const content: BlocksContent = article?.attributes?.body ?? [];
 
+    let coverImage =
+        article?.attributes?.coverImage
+            ?.data?.attributes?.url;
+
+    if (HOST_URL && coverImage != undefined) {
+        coverImage = HOST_URL + coverImage;
+    } else {
+        coverImage = "";
+    }
+
     return (
-        <>
+        <div className="flex items-center justify-center">
+          <BlogCard
+            className={
+                article
+                    ? "m-20 mt-10 w-full pb-5"
+                    : "m-[1%] mb-5 min-w-80 max-w-[30%] grow pb-5"
+            }
+            key={article.id}
+        >
             <div className="flex flex-col items-center gap-4">
                 <h1 className="text-4xl font-extrabold">
                     {article?.attributes?.title}
@@ -108,10 +129,19 @@ export default async function Page({
                 <p className="text-sm text-muted-foreground">
                     {article?.attributes?.subtitle}{" "}
                 </p>
+                <div className="relative h-[300px] w-full">
+                  <Image 
+                      src={coverImage} 
+                      alt="coverImage" 
+                      className="absolute inset-0 object-contain"
+                      layout="fill"
+                  />
+              </div>
                 <div className="w-1/2">
                     <BlockRendererClient content={content} />
                 </div>
             </div>
-        </>
+          </BlogCard>
+        </div>
     );
 }
