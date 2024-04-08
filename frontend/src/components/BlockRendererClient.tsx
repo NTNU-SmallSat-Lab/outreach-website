3f2c9e4c55468ebcaeb8e7366f0a8b4bfa1f2eb9
Lucas Tran <lucastra@stud.ntnu.no>
2024-04-08 16:37:45 +0200

    style(frontend): :lipstick: Wrap div around blocksrenderer component and add typography tailwind styling for all blocksrendered content

--- frontend/src/components/BlockRendererClient.tsx
+++ frontend/src/components/BlockRendererClient.tsx
@@ -13,78 +13,80 @@ export default function BlockRendererClient({
 }) {
     if (!content) return null;
     return (
-        <BlocksRenderer
-            content={content}
-            blocks={{
-                image: ({ image }) => {
-                    return (
-                        <NextImage
-                            src={image.url}
-                            width={image.width}
-                            className="object-contain"
-                            height={image.height}
-                            alt={image.alternativeText || ""}
-                        />
-                    );
-                },
-                heading: ({ children, level }) => {
-                    switch (level) {
-                        case 1:
-                            return <h1>{children}</h1>;
-                        case 2:
-                            return <h2>{children}</h2>;
-                        case 3:
-                            return <h3>{children}</h3>;
-                        case 4:
-                            return <h4>{children}</h4>;
-                        case 5:
-                            return <h5>{children}</h5>;
-                        default:
-                            return <h1>{children}</h1>;
-                    }
-                },
-
-                paragraph: ({ children }) => {
-                    const child = children as {
-                        props: {
-                            type: "text";
-                            text: string;
-                            italic: boolean;
-                        };
-                    }[];
-
-                    const italic = child[0]?.props.italic;
-                    if (italic) {
-                        return <p className="text-sm">{children}</p>;
-                    }
-                    return <p>{children}</p>;
-                },
-
-                link: ({ url, children }) => {
-                    // Check if the URL is a valid YouTube video link
-                    const isYouTubeVideo = url.includes("youtube.com");
-                    // Render the iframe only if it's a YouTube video link
-                    if (isYouTubeVideo) {
+        <div className="prose prose-invert lg:prose-xl">
+            <BlocksRenderer
+                content={content}
+                blocks={{
+                    image: ({ image }) => {
                         return (
-                            <div className="flex w-full items-center">
-                                <iframe
-                                    width={"100%"}
-                                    className="aspect-video"
-                                    src={url}
-                                    title="YouTube video"
-                                ></iframe>
-                            </div>
+                            <NextImage
+                                src={image.url}
+                                width={image.width}
+                                className="object-contain"
+                                height={image.height}
+                                alt={image.alternativeText || ""}
+                            />
                         );
-                    } else {
-                        // If it's not a video link, return an anchor with the href
-                        return (
-                            <a href={url} target="_blank">
-                                {children}
-                            </a>
-                        );
-                    }
-                },
-            }}
-        />
+                    },
+                    heading: ({ children, level }) => {
+                        switch (level) {
+                            case 1:
+                                return <h1>{children}</h1>;
+                            case 2:
+                                return <h2>{children}</h2>;
+                            case 3:
+                                return <h3>{children}</h3>;
+                            case 4:
+                                return <h4>{children}</h4>;
+                            case 5:
+                                return <h5>{children}</h5>;
+                            default:
+                                return <h1>{children}</h1>;
+                        }
+                    },
+
+                    paragraph: ({ children }) => {
+                        const child = children as {
+                            props: {
+                                type: "text";
+                                text: string;
+                                italic: boolean;
+                            };
+                        }[];
+
+                        const italic = child[0]?.props.italic;
+                        if (italic) {
+                            return <p>{children}</p>;
+                        }
+                        return <p >{children}</p>;
+                    },
+
+                    link: ({ url, children }) => {
+                        // Check if the URL is a valid YouTube video link
+                        const isYouTubeVideo = url.includes("youtube.com");
+                        // Render the iframe only if it's a YouTube video link
+                        if (isYouTubeVideo) {
+                            return (
+                                <div className="flex w-full items-center">
+                                    <iframe
+                                        width={"100%"}
+                                        className="aspect-video"
+                                        src={url}
+                                        title="YouTube video"
+                                    ></iframe>
+                                </div>
+                            );
+                        } else {
+                            // If it's not a video link, return an anchor with the href
+                            return (
+                                <a href={url} target="_blank">
+                                    {children}
+                                </a>
+                            );
+                        }
+                    },
+                }}
+            />
+        </div>
     );
 }
