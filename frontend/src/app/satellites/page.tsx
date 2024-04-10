a22fe27f619e24d1c40f59e8ab61508a380b47c8
Lucas Tran <lucastra@stud.ntnu.no>
2024-04-10 15:43:19 +0200

    refactor(frontend): :recycle: Run prettier

--- frontend/src/app/satellites/page.tsx
+++ frontend/src/app/satellites/page.tsx
@@ -46,27 +46,34 @@ export default async function Satellites() {
                         Theese are all the satellites we have worked on.
                     </PageSubtitle>
                 </PageHeaderAndSubtitle>
-            <div className="flex flex-wrap justify-center gap-4 md:justify-start">
-                {graphqlData?.data?.satellites?.data?.map((satellite: any) => {
-                    let previewImage =
-                        satellite?.attributes?.previewImage?.data?.attributes
-                            ?.url;
-                    if (OUTSIDE_STRAPI_URL && previewImage != undefined) {
-                        previewImage = OUTSIDE_STRAPI_URL + previewImage;
-                    }
-                    let satelliteName = satellite?.attributes?.name ?? "";
-                    let missionStatus =
-                        satellite?.attributes?.missionStatus ?? "";
-                    return (
-                        // eslint-disable-next-line react/jsx-key
-                        <SatelliteCard
-                            satelliteName={satelliteName}
-                            missionStatus={missionStatus}
-                            previewImage={previewImage}
-                            satelliteId={satellite.id}
-                        ></SatelliteCard>
-                    );
-                })}
+                <div className="flex flex-wrap justify-center gap-4 md:justify-start">
+                    {graphqlData?.data?.satellites?.data?.map(
+                        (satellite: any) => {
+                            let previewImage =
+                                satellite?.attributes?.previewImage?.data
+                                    ?.attributes?.url;
+                            if (
+                                OUTSIDE_STRAPI_URL &&
+                                previewImage != undefined
+                            ) {
+                                previewImage =
+                                    OUTSIDE_STRAPI_URL + previewImage;
+                            }
+                            let satelliteName =
+                                satellite?.attributes?.name ?? "";
+                            let missionStatus =
+                                satellite?.attributes?.missionStatus ?? "";
+                            return (
+                                // eslint-disable-next-line react/jsx-key
+                                <SatelliteCard
+                                    satelliteName={satelliteName}
+                                    missionStatus={missionStatus}
+                                    previewImage={previewImage}
+                                    satelliteId={satellite.id}
+                                ></SatelliteCard>
+                            );
+                        },
+                    )}
                 </div>
             </div>
         );
--- frontend/src/components/ui/satelliteCard.tsx
+++ frontend/src/components/ui/satelliteCard.tsx
@@ -19,51 +19,43 @@ const SatelliteCard: React.FC<SatelliteCardProps> = ({
     satelliteName,
     missionStatus,
     previewImage,
-    satelliteId
-    
+    satelliteId,
 }) => {
     /*const setSelectedSatellite = useSatelliteStore(
         (state) => state.setSelectedSatellite,
     );*/
 
-
-
     return (
-            <Link
-                href={
-                    "/satellites/" +
-                    satelliteName
-                }
-                className="w-1/1.5 transition-transform duration-300 ease-in-out hover:scale-105 hover:transform md:w-1/3"
-                key={satelliteId.toString()}
-            >
-                <Card className="flex h-full w-full flex-col">
-                    <CardHeader className="flex flex-col items-center justify-center">
-                        <CardTitle>
-                            {satelliteName}
-                        </CardTitle>
-                    </CardHeader>
-                    <CardContent className="flex flex-col items-center">
-                        <SatelliteStatsTable
-                            satName={satelliteName}
-                            missionStatus={missionStatus}
+        <Link
+            href={"/satellites/" + satelliteName}
+            className="w-1/1.5 transition-transform duration-300 ease-in-out hover:scale-105 hover:transform md:w-1/3"
+            key={satelliteId.toString()}
+        >
+            <Card className="flex h-full w-full flex-col">
+                <CardHeader className="flex flex-col items-center justify-center">
+                    <CardTitle>{satelliteName}</CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col items-center">
+                    <SatelliteStatsTable
+                        satName={satelliteName}
+                        missionStatus={missionStatus}
+                    />
+                    {previewImage ? (
+                        <Image
+                            src={previewImage}
+                            alt={previewImage}
+                            width={200}
+                            height={0}
+                            className="margin p-2"
                         />
-                        {previewImage ? (
-                            <Image
-                                src={previewImage}
-                                alt={previewImage}
-                                width={200}
-                                height={0}
-                                className="margin p-2"
-                            />
-                        ) : (
-                            <div className="m-0 flex aspect-video max-h-full max-w-full items-center justify-center object-contain">
-                                <OuiImage />
-                            </div>
-                        )}
-                    </CardContent>
-                </Card>
-            </Link> 
+                    ) : (
+                        <div className="m-0 flex aspect-video max-h-full max-w-full items-center justify-center object-contain">
+                            <OuiImage />
+                        </div>
+                    )}
+                </CardContent>
+            </Card>
+        </Link>
     );
 };
 
