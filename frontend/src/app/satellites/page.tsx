1aa55dd89574182b1faa85ab5bf626ccc842b887
Mads Hermansen <119772939+madshermansen@users.noreply.github.com>
2024-04-10 16:39:39 +0200

    202 fikse styling satellite page borders (#251)
    * emoji
    
    * feat(frontend): commit for merge
    
    * feat(frontend): :sparkles: Refactor satellite page and add SatelliteStatsTableRow component
    
    * refactor(frontend): :art: Refactor SVG component and update table layout
    
    * build(ci/cd): :rocket: Add country-emoji package to frontend dependencies

--- frontend/src/app/satellites/page.tsx
+++ frontend/src/app/satellites/page.tsx
@@ -1,13 +1,6 @@
 import { gql } from "@/__generated__/gql";
+import SatelliteStatsTableRow from "@/components/satelliteData/SatelliteStatsTableRow";
 import { getClient } from "@/lib/ApolloClient";
-import SatelliteCard from "@/components/ui/satelliteCard";
-import {
-    PageHeader,
-    PageHeaderAndSubtitle,
-    PageSubtitle,
-} from "@/components/PageHeader";
-
-const STRAPI_URL = process.env.STRAPI_URL;
 const GET_SATELLITES = gql(`
 query GET_SATELLITES {
     satellites {
@@ -38,38 +31,35 @@ export default async function Satellites() {
         });
 
         return (
-            <div>
-                <PageHeaderAndSubtitle>
-                    <PageHeader>Satellites</PageHeader>
-                    <PageSubtitle>
-                        Theese are all the satellites we have worked on.
-                    </PageSubtitle>
-                </PageHeaderAndSubtitle>
-                <div className="flex flex-wrap justify-center gap-4 md:justify-start">
-                    {graphqlData?.data?.satellites?.data?.map(
-                        (satellite: any) => {
-                            let previewImage =
-                                satellite?.attributes?.previewImage?.data
-                                    ?.attributes?.url;
-                            if (STRAPI_URL && previewImage != undefined) {
-                                previewImage = STRAPI_URL + previewImage;
-                            }
-                            let satelliteName =
-                                satellite?.attributes?.name ?? "";
-                            let missionStatus =
-                                satellite?.attributes?.missionStatus ?? "";
-                            return (
-                                // eslint-disable-next-line react/jsx-key
-                                <SatelliteCard
-                                    satelliteName={satelliteName}
-                                    missionStatus={missionStatus}
-                                    previewImage={previewImage}
-                                    satelliteId={satellite.id}
-                                ></SatelliteCard>
-                            );
-                        },
-                    )}
-                </div>
+            <div className="flex w-full flex-col items-center justify-center">
+                <h1 className="my-10 text-4xl font-extrabold  text-white">
+                    Satellites
+                </h1>
+                <table className="w-4/5 table-auto border-collapse rounded-md border-b border-white shadow">
+                    <thead>
+                        <tr className="border-y border-white px-3 py-2 text-left text-white">
+                            <th className="px-6">Satellite</th>
+                            <th className="px-6">Speed</th>
+                            <th className="px-6">Altitude</th>
+                            <th className="px-6">Latitude</th>
+                            <th className="px-6">Longitude</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {graphqlData?.data?.satellites?.data?.map(
+                            (satellite: any) => {
+                                let satelliteName =
+                                    satellite?.attributes?.name ?? "";
+                                return (
+                                    <SatelliteStatsTableRow
+                                        key={satellite.id}
+                                        satName={satelliteName}
+                                    />
+                                );
+                            },
+                        )}
+                    </tbody>
+                </table>
             </div>
         );
     } catch (error) {
---
+++ frontend/src/components/satelliteData/SatelliteStatsTableRow.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { useState, useEffect } from "react";
+import { convertSatrec, SatelliteInfo } from "@/lib/convertSatrec";
+import { useSatelliteStore } from "@/lib/store";
+import Link from "next/link";
+
+const updateInterval = 10;
+
+export default function SatelliteStatsTableRow({
+    satName,
+}: {
+    satName: string;
+}) {
+    const { satelliteData, fetchAndSetSatelliteData } = useSatelliteStore();
+    const [satelliteInfo, setSatelliteInfo] = useState<SatelliteInfo | null>(
+        null,
+    );
+
+    // Fetch satellite data on component mount
+    useEffect(() => {
+        fetchAndSetSatelliteData(satName);
+    }, [fetchAndSetSatelliteData, satName]);
+
+    // Update satellite info every `updateInterval` ms
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            // Access satellite data by name
+            const satData = satelliteData[satName];
+            if (satData) {
+                const updatedInfo = convertSatrec(satData.satrec, satData.name);
+                setSatelliteInfo(updatedInfo);
+            }
+        }, updateInterval);
+
+        // Clear interval on component unmount
+        return () => clearInterval(intervalId);
+    }, [satelliteData, satName]);
+
+    // Display loading message if satellite info is not available
+    if (!satelliteInfo) {
+        return (
+            <tr>
+                <td className="px-6 py-4">Loading...</td>
+                <td className="px-6 py-4">Loading...</td>
+                <td className="px-6 py-4">Loading...</td>
+                <td className="px-6 py-4">Loading...</td>
+                <td className="px-6 py-4">Loading...</td>
+            </tr>
+        );
+    }
+    return (
+        <tr className="duration-150 hover:bg-white hover:text-black">
+            <td className="px-6 py-4">
+                <Link href={`/satellites/${satName}`} className="w-full">
+                    {satName}
+                </Link>
+            </td>
+            <td className="px-6 py-4">
+                <Link href={`/satellites/${satName}`} className="w-full">
+                    {satelliteInfo.velocity + " km/s"}
+                </Link>
+            </td>
+            <td className="px-6 py-4">
+                <Link href={`/satellites/${satName}`} className="w-full">
+                    {satelliteInfo.altitude + " km"}
+                </Link>
+            </td>
+            <td className="px-6 py-4">
+                <Link href={`/satellites/${satName}`} className="w-full">
+                    {satelliteInfo.latitudeDeg + "° N"}
+                </Link>
+            </td>
+            <td className="px-6 py-4">
+                <Link href={`/satellites/${satName}`} className="w-full">
+                    {satelliteInfo.longitudeDeg + "° E"}
+                </Link>
+            </td>
+        </tr>
+    );
+}
