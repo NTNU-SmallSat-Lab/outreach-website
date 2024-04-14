f776cb2059b30759a7e936522a31e573b35a10cd
Lucas Tran <lucastran1107@gmail.com>
2024-04-14 03:18:22 +0200

    refactor(frontend): :recycle: Run prettier

--- frontend/src/components/RelatedProjectsAndSatellites.tsx
+++ frontend/src/components/RelatedProjectsAndSatellites.tsx
@@ -5,7 +5,6 @@ const STRAPI_URL = process.env.STRAPI_URL;
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { OuiImage } from "@/components/fullBlogCard";
 
-
 export default function RelatedProjectsAndSatellites({
     project,
 }: {
