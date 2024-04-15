b38417ad7bf1385ed1efeff9134f27f468e160df
Jakob Grøtan Gregusson <jakobgg@stud.ntnu.no>
2024-04-15 15:13:39 +0200

    test(frontend): :test_tube: Added failing mobile project test

--- frontend/tests/project.mobile.spec.ts
+++ frontend/tests/project.mobile.spec.ts
@@ -3,8 +3,12 @@ import { test, expect } from "@playwright/test";
 //BASE_URL: http://web.hypso.ies.ntnu.no:3000/
 
 test.describe("All Project Page Test", () => {
+    test.use({
+        viewport: { width: 390, height: 844 },
+    });
     test("CheckHeadingAndSubtitleIsVisible", async ({ page }) => {
         await page.goto("/");
+        await page.getByRole("navigation").getByRole("button").click();
         await page.getByRole('button', { name: 'Projects' }).click();
         await expect(page).toHaveURL("/projects");
         await expect(page.getByTestId('pageHeader')).toBeVisible();
@@ -12,6 +16,7 @@ test.describe("All Project Page Test", () => {
     });
     test("CheckIfCardIsClickable", async ({ page }) => {
         await page.goto("/");
+        await page.getByRole("navigation").getByRole("button").click();
         await page.getByRole('button', { name: 'Projects' }).click();
         await expect(page).toHaveURL("/projects");
         await expect(page.getByTestId('projectCard').first()).toBeVisible();
@@ -19,11 +24,16 @@ test.describe("All Project Page Test", () => {
         //Checks if the URL matches /projects/anySlug
         await expect(page).toHaveURL(/\/projects\/.+$/);
     });
+    
 });
 
 test.describe("Individual Project Page Test", () => {
+    test.use({
+        viewport: { width: 390, height: 844 },
+    });
     test("TestHeadingAndParagraphIsVisible", async ({ page }) => {
         await page.goto("/");
+        await page.getByRole("navigation").getByRole("button").click();
         await page.getByRole('button', { name: 'Projects' }).click();
         await page.getByTestId("projectCard").first().click();
         await expect(page).toHaveURL(/\/projects\/.+$/); 
