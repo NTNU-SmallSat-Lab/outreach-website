94a5305c69c969a994ef7700f4c2ba232aa9755a
EliasKnudsen <eliasskn@stud.ntnu.no>
2024-04-15 13:55:11 +0200

    Fix since launch time

--- frontend/src/components/ui/launchDateCountDown.tsx
+++ frontend/src/components/ui/launchDateCountDown.tsx
@@ -13,6 +13,7 @@ const LaunchDateCountDown: React.FC<LaunchDateCountDownProps> = ({
     const [hasLaunched, setHasLaunched] = useState<boolean | undefined>(
         undefined,
     );
+    const [columns, setColumns] = useState<string>();
 
     useEffect(() => {
         if (!launchDateString) return;
@@ -21,8 +22,8 @@ const LaunchDateCountDown: React.FC<LaunchDateCountDownProps> = ({
 
         const intervalId = setInterval(() => {
             const now = new Date();
-            const difference = launchDate.getTime() - now.getTime();
-
+            const difference = Math.abs(launchDate.getTime() - now.getTime());
+            const differenceReal = launchDate.getTime() - now.getTime();
             const days = Math.floor(difference / (1000 * 60 * 60 * 24));
             const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
             const minutes = Math.floor((difference / 1000 / 60) % 60);
@@ -37,7 +38,8 @@ const LaunchDateCountDown: React.FC<LaunchDateCountDownProps> = ({
             timeParts.push(`${seconds} seconds`);
 
             setDisplayTime(timeParts);
-            setHasLaunched(difference <= 0);
+            setHasLaunched(differenceReal <= 0);
+            setColumns("grid " + "grid-cols-" + timeParts.length.toString());
         }, 1000);
 
         return () => clearInterval(intervalId);
@@ -64,9 +66,9 @@ const LaunchDateCountDown: React.FC<LaunchDateCountDownProps> = ({
                 <div className="rounded-lg p-4 text-center text-4xl tracking-widest text-white shadow-md">
                     {displayTime.length > 0 && (
                         <div className="flex justify-center">
-                            <div className="grid grid-cols-3">
+                            <div className={columns}>
                                 {displayTime.map((part, index) => (
-                                    <div key={index} className="px-3">
+                                    <div key={index} className="px-2">
                                         <p>{part.split(" ")[0]}</p>
                                         <p className="text-sm">
                                             {part.split(" ")[1]}
