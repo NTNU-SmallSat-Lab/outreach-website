2677e0ee1d0124581e9ce7f745cc3d2a9f74b41c
Mads Hermansen <mads.d.hermansen+main@gmail.com>
2024-04-16 17:35:00 +0200

    feat(frontend): :sparkles: prettier write

--- frontend/src/app/satellites/[satelliteSlug]/page.tsx
+++ frontend/src/app/satellites/[satelliteSlug]/page.tsx
@@ -42,7 +42,7 @@ export default async function SatelliteInfoPage({
                         launchDateString={satelliteInfo.launchDate}
                     ></LaunchDateCountDown>
                 </div>
-                <h1 className="self-start text-center text-4xl font-bold w-full md:text-left my-3">
+                <h1 className="my-3 w-full self-start text-center text-4xl font-bold md:text-left">
                     {satelliteInfo.name}
                 </h1>
                 {/* Container for satname, stats and sat image */}
