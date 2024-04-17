591c62cc6510970c06f7a976153bd18b6723cedd
Mats <mnyflot@gmail.com>
2024-04-17 16:01:24 +0200

    test(frontend): :white_check_mark: Make test for blogs pages. Edit featured image mobile test to use correct viewport

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -9,8 +9,7 @@ import { defineConfig, devices } from "@playwright/test";
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-const baseURL = `http://localhost:3000/`;
-//const baseURL = `http://web.hypso.ies.ntnu.no:3000/`;
+const baseURL = `http://web.hypso.ies.ntnu.no:3000/`;
 
 export default defineConfig({
     testDir: "./tests",
@@ -19,7 +18,7 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    retries: 2,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
