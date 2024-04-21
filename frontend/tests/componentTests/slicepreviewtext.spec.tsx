bf1d1677fb5c9f4a45269aaf1549624bfb54fd36
luctra02 <64017398+luctra02@users.noreply.github.com>
2024-04-21 19:28:18 +0200

    341 create component test for slicepreviewtext (#342)
    * test(frontend): :test_tube: write component test for sharebuttons
    
    * test(frontend): :test_tube: write test for slicepreviewtest to check if the component slices the 100 first chars in the first paragraph

---
+++ frontend/tests/componentTests/slicepreviewtext.spec.tsx
@@ -0,0 +1,53 @@
+import { test, expect } from "@playwright/experimental-ct-react";
+import { SlicePreviewText } from "@/components/SlicePreviewText";
+import { BlocksContent } from "@strapi/blocks-react-renderer";
+
+test("Check SlicePreviewText function", async () => {
+    // Mock content with a paragraph block
+    const mockContent: BlocksContent = [
+        {
+            type: "heading",
+            children: [
+                {
+                    type: "text",
+                    text: "Lorem ipsum",
+                },
+            ],
+            level: 1,
+        },
+        {
+            type: "paragraph",
+            children: [
+                {
+                    type: "text",
+                    text: "",
+                },
+            ],
+        },
+        {
+            type: "paragraph",
+            children: [
+                {
+                    type: "text",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pulvinar viverra diam. Aliquam venenatis fermentum nisl, ut pulvinar nunc consectetur ac. Nunc sit amet velit nibh. Sed sagittis mauris in elit varius aliquet. Phasellus lacus risus, suscipit sed nisl ac, consectetur faucibus est. Curabitur porta ante nec tortor finibus venenatis. Etiam ac nulla vitae est dapibus tincidunt. Quisque mollis pulvinar bibendum.",
+                },
+            ],
+        },
+        {
+            type: "paragraph",
+            children: [
+                {
+                    type: "text",
+                    text: "Do not slice this paragraph, only the first one",
+                },
+            ],
+        },
+    ];
+
+    const slicedText = SlicePreviewText(mockContent);
+
+    // Assert that the sliced text contains the expected content
+    expect(slicedText).toContain(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pulvinar viverra diam. Aliquam v...",
+    );
+});
