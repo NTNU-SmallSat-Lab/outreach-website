7725e12eec039657f6d6e56bb1301745ab388d50
Mats <mnyflot@gmail.com>
2024-04-29 14:18:56 +0200

    test(frontend): :white_check_mark: make statstablerow test

--- frontend/tests/componentTests/satelliteStatsTableRow.spec.tsx
+++ frontend/tests/componentTests/satelliteStatsTableRow.spec.tsx
@@ -1,53 +1,24 @@
 import SatelliteStatsTableRow from "@/components/satelliteData/SatelliteStatsTableRow";
 import { test, expect } from "@playwright/experimental-ct-react";
-import { useRouter } from "next/navigation";
 
 // Mock satellite data
 const mockSatellite = {
     id: "satelliteId",
     name: "Satellite Name",
     previewImage: "Satellite Image",
-    slug: "StaelliteSlug",
+    slug: "SatelliteSlug",
 };
 
-const mockGlobeData = {
-    "features": [
-      {
-        "geometry": {
-          "type": "Polygon",
-          "coordinates": [
-            [
-              [10, 10], [20, 10], [20, 20], [10, 20], [10, 10] 
-            ]
-          ]
-        },
-        "properties": {
-          "scalerank": 1,
-          "featurecla": "Admin-0 country",
-          "ADMIN": "Test Country",
-        }
-      }
-    ]
-  }
-
-  const router = {
-    push: jest.fn() 
-};
-useRouter.mockReturnValue(router);
-
-
-test("should work", async ({ mount }) => {
-    const handleRowClick = (slug: string) => {
-        // This will update the URL without triggering a full page refresh
-        router.push(`/satellites/${slug}`, undefined);
-    };
-
+test("should be clickable", async ({ mount }) => {
     const component = await mount(
-        <SatelliteStatsTableRow 
-            satName={mockSatellite.name}  
-            handleRowClick={() => handleRowClick(mockSatellite.slug)}
+        <SatelliteStatsTableRow
+            satName={mockSatellite.name}
+            handleRowClick={() => {}}
         />,
     );
 
     await expect(component).toContainText("Satellite Name");
+
+    // Click the row
+    await component.click();
 });
