5880bf290ca4a211e4fd8e07fca71c35f578b340
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-05-07 22:27:51 +0200

    remove any typecast

--- frontend/src/app/projects/page.tsx
+++ frontend/src/app/projects/page.tsx
@@ -74,7 +74,7 @@ export default async function ProjectsPage() {
             </div>
 
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-                {graphqlData.data.projects.data.map((project: any) => {
+                {graphqlData.data.projects.data.map((project) => {
                     let previewImage =
                         project?.attributes?.previewImage?.data?.attributes
                             ?.url;
