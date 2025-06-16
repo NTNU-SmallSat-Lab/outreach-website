29d03745858b670410d0607511289fd9e869faef
Thibault Camlane <thibault.camlane@gmail.com>
2025-06-16 15:31:59 +0200

    test: for changing satellite prediction time

--- frontend/tests/e2e/satellite.desktop.spec.ts
+++ frontend/tests/e2e/satellite.desktop.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { Sliders } from "lucide-react";
 
 test.describe("All Satellites Page Test", () => {
     test("CheckHeadingAndSubtitleIsVisible", async ({ page }) => {
@@ -30,5 +31,16 @@ test.describe("Individual Satellite Page Test", () => {
         await expect(page.getByRole("heading").first()).toBeVisible();
         await expect(page.getByRole("heading").nth(2)).toBeVisible();
         await expect(page.getByTestId("blockParagraph").first()).toBeVisible();
+    });
+
+    test("TestChangingPredictionTime", async ({ page }) => {
+        await page.goto("/");
+        await page.getByRole("button", { name: "Satellites" }).click();
+        await page.getByTestId("satellitesTableRow").first().click();
+        await expect(page).toHaveURL(/\/satellites\/.+$/);
+        const slider = page.getByTestId("prediction-slider");
+        await expect(slider).toHaveValue("120");
+        await slider.fill("180"); //Prediction of position in 180 minutes
+        await expect(slider).toHaveValue("180");
     });
 });
