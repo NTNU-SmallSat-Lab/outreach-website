ba199bd5494028845171660ee4857d7af99311a8
Thibault Camlane <thibault.camlane@gmail.com>
2025-06-17 14:31:42 +0200

    clean: unused code cleaned up

--- frontend/src/app/satellites/[satelliteSlug]/page.tsx
+++ frontend/src/app/satellites/[satelliteSlug]/page.tsx
@@ -16,24 +16,6 @@ import { getClient } from "@/lib/ApolloClient";
 import OrbitDataGraph from "./orbitDataGraph";
 import Render3DMod from "../render3DMod";
 
-// Add TypeScript support for the 'model-viewer' custom element
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            "model-viewer": React.DetailedHTMLProps<
-                React.HTMLAttributes<HTMLElement>,
-                HTMLElement
-            > & {
-                src?: string;
-                alt?: string;
-                "auto-rotate"?: boolean | string;
-                "camera-controls"?: boolean | string;
-                style?: React.CSSProperties;
-            };
-        }
-    }
-}
-
 export interface ProjectOrSatellite {
     id: string;
     title: string;
